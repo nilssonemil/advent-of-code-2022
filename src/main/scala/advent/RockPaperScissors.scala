bcc24c906b10e854975f3fdac76bbfe7f573d0cd
Emil Nilsson <emil.nilsson@protonmail.com>
2022-12-02 06:58:18 +0100

    Day 2 part 2

--- src/main/scala/advent/RockPaperScissors.scala
+++ src/main/scala/advent/RockPaperScissors.scala
@@ -3,44 +3,61 @@ package solutions.shitops.advent
 import solutions.shitops.advent.util.ResourceReader
 
 sealed trait HandShape
-case object Rock extends HandShape 
-case object Paper extends HandShape
+case object Rock     extends HandShape
+case object Paper    extends HandShape
 case object Scissors extends HandShape
 
-case class Game(opponent: HandShape, me: HandShape) {
+sealed trait Outcome
+case object Win  extends Outcome
+case object Lose extends Outcome
+case object Draw extends Outcome
+
+case class Game(opponent: HandShape, outcome: Outcome) {
+
+  private def handshape(desired: Outcome, opponent: HandShape): HandShape =
+    (desired, opponent) match {
+      case (Win, Rock)      => Paper
+      case (Win, Paper)     => Scissors
+      case (Win, Scissors)  => Rock
+      case (Draw, opponent) => opponent
+      case (Lose, Rock)     => Scissors
+      case (Lose, Paper)    => Rock
+      case (Lose, Scissors) => Paper
+    }
+
   def score = {
-    val shape = me match {
-      case Rock => 1
-      case Paper => 2
+    val me           = handshape(outcome, opponent)
+    val shapeScore   = me match {
+      case Rock     => 1
+      case Paper    => 2
       case Scissors => 3
     }
-    val outcome = (me, opponent) match {
-      case (me, opponent) if me == opponent => 3
-      case (Rock, Scissors) => 6
-      case (Paper, Rock) => 6
-      case (Scissors, Paper) => 6
-      case (_, __) => 0
+    val outcomeScore = outcome match {
+      case Win  => 6
+      case Draw => 3
+      case Lose => 0
     }
-    shape + outcome
+    shapeScore + outcomeScore
   }
 }
 
-def read = ResourceReader.readLines("rock-paper-scissors.input")
+def read  = ResourceReader.readLines("rock-paper-scissors.input")
 def games = read.map(str => {
   val Array(opponent, me) = str.split(" ")
-  Game(opponent match {
-    case "A" => Rock
-    case "B" => Paper
-    case "C" => Scissors
-  },
-  me match {
-    case "X" => Rock
-    case "Y" => Paper
-    case "Z" => Scissors
-  })
+  Game(
+    opponent match {
+      case "A" => Rock
+      case "B" => Paper
+      case "C" => Scissors
+    },
+    me match {
+      case "X" => Lose
+      case "Y" => Draw
+      case "Z" => Win
+    },
+  )
 })
 
 def score = games.map(_.score).sum
 
 @main def totalScore = println(f"Score: $score")
-
--- src/test/scala/advent/RockPaperScissorsSuite.scala
+++ src/test/scala/advent/RockPaperScissorsSuite.scala
@@ -6,56 +6,15 @@ import solutions.shitops.advent.util.ResourceReader
 
 class RockPaperScissorsSuite extends AnyFunSuite {
 
-
-  sealed trait HandShape
-  case object Rock extends HandShape 
-  case object Paper extends HandShape
-  case object Scissors extends HandShape
-
-  case class Game(opponent: HandShape, me: HandShape) {
-    def score = {
-      val shape = me match {
-        case Rock => 1
-        case Paper => 2
-        case Scissors => 3
-      }
-      val outcome = (me, opponent) match {
-        case (me, opponent) if me == opponent => 3
-        case (Rock, Scissors) => 6
-        case (Paper, Rock) => 6
-        case (Scissors, Paper) => 6
-        case (_, __) => 0
-      }
-      shape + outcome
-    }
-  }
-
-  def read = ResourceReader.readLines("rock-paper-scissors.input")
-  def games = read.map(str => {
-    val Array(opponent, me) = str.split(" ")
-    Game(opponent match {
-      case "A" => Rock
-      case "B" => Paper
-      case "C" => Scissors
-    },
-    me match {
-      case "X" => Rock
-      case "Y" => Paper
-      case "Z" => Scissors
-    })
-  })
-
-  def score = games.map(_.score).sum
-  
   test("input is read ok") {
     assert(read == List("A Y", "B X", "C Z"))
   }
 
   test("input converted to game") {
-    assert(games == List(Game(Rock, Paper), Game(Paper, Rock), Game(Scissors, Scissors)))
+    assert(games == List(Game(Rock, Draw), Game(Paper, Lose), Game(Scissors, Win)))
   }
 
   test("total score") {
-    assert(score == 15)
+    assert(score == 12)
   }
 }
