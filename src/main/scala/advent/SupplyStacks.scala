e24b4ce8282f486a620b955fb5ebf444d41dca57
Emil Nilsson <emil.nilsson@protonmail.com>
2022-12-05 23:47:49 +0100

    Refactor day 5

--- src/main/scala/advent/SupplyStacks.scala
+++ src/main/scala/advent/SupplyStacks.scala
@@ -4,51 +4,50 @@ import solutions.shitops.advent.util.ResourceReader
 import scala.collection.mutable.Stack
 
 object SupplyStacks {
-  val Array(crates, procedure) = ResourceReader
+  case class Move(from: Int, to: Int, amount: Int)
+  class Stacks(drawing: String) {
+    val lines   = drawing.split("\n")
+    val indexes = lines.last.zipWithIndex
+      .map((char, index) => if (char.isWhitespace) 0 else index)
+      .filterNot(_ == 0)
+      .toList
+    val crates  = lines.dropRight(1).toList
+    val stacks  =
+      for (index <- indexes)
+        yield Stack[Char]().pushAll(
+          crates.map(str => str(index)).filterNot(_.isWhitespace).reverse.toList,
+        )
+
+    def moveCrates(move: Move) = {
+      val from   = move.from - 1
+      val to     = move.to - 1
+      var toPush = Stack[Char]()
+      1 to move.amount foreach { _ =>
+        val popped = stacks(from).pop()
+        toPush.push(popped)
+      }
+      toPush.foreach(stacks(to).push(_))
+    }
+
+    def tops = stacks.map(_.head).mkString
+  }
+
+  val Array(drawing, procedure) = ResourceReader
     .readString("supply-stacks.input")
     .split("\n\n")
-  val lines                    = crates.split("\n")
-  val idsLine                  = lines.last
-  val ids                      = idsLine.trim.split("   ").map(_.toInt).toList
-  val onlyCrates               = lines.dropRight(1).toList
-  val indexes                  = idsLine.zipWithIndex
-    .map((char, index) => {
-      if (char.isWhitespace) {
-        0
-      } else {
-        index
-      }
-    })
-    .filterNot(_ == 0)
-    .toList
-  val stacks                   =
-    for (index <- indexes)
-      yield Stack[Char]().pushAll(
-        onlyCrates
-          .map(str => str(index))
-          .filterNot(_.isWhitespace)
-          .reverse
-          .toList,
-      )
-  case class Move(from: Int, to: Int, amount: Int)
-  val moves                    = procedure
+  val stacks                    = Stacks(drawing)
+  val moves                     = procedure
     .split("\n")
     .map(line => {
       val Array(_, amount, __, from, ___, to) = line.split(" ")
       Move(from.toInt, to.toInt, amount.toInt)
     })
     .toList
-  moves.foreach(move => {
-    val from   = move.from - 1
-    val to     = move.to - 1
-    var toPush = Stack[Char]()
-    1 to move.amount foreach { _ =>
-      val popped = stacks(from).pop()
-      toPush.push(popped)
-    }
-    toPush.foreach(stacks(to).push(_))
-  })
-  val tops                     = stacks.map(_.head).mkString
+
+  def tops = {
+    moves.foreach(stacks.moveCrates(_))
+    stacks.tops
+  }
 }
 
 @main def tops = println(f"tops: \"${SupplyStacks.tops}\"")
